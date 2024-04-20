7813f1c89e455b76e6d8ffd87386b3a9da69ffe0
buckapi <buckapi@localhost.localdomain>
2024-04-20 09:41:36 -0500

    environments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    apirestUrl: 'http://localhost:8090/api/',
-    apiUrl: 'http://localhost:8090/api/collections/images/records'
+    apirestUrl: 'https://db.buckapi.com:8090/api/',
+    apiUrl: 'https://db.buckapi.com:8090/api/collections/images/records'
   };
